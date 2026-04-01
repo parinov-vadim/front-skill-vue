2cc6893b5cbcd17c1c230539730f0ad367a48636
Vadim Parinov <vadimparinov2014@gmail.com>
2026-04-01 20:04:56 +0300

    add indexnow

---
+++ server/routes/indexnow.post.ts
@@ -0,0 +1,24 @@
+const INDEXNOW_KEY = '16e430d9e352240914cbeab6c28630bf'
+const INDEXNOW_ENDPOINT = 'https://api.indexnow.org/indexnow'
+const SITE_HOST = 'frontskill.ru'
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody<{ urls: string[] }>(event)
+
+  if (!body?.urls?.length) {
+    throw createError({ statusCode: 400, statusMessage: 'urls array is required' })
+  }
+
+  const response = await $fetch(INDEXNOW_ENDPOINT, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: {
+      host: SITE_HOST,
+      key: INDEXNOW_KEY,
+      keyLocation: `https://${SITE_HOST}/${INDEXNOW_KEY}.txt`,
+      urlList: body.urls,
+    },
+  })
+
+  return { success: true, submitted: body.urls.length, response }
+})
